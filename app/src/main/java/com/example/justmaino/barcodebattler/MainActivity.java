7f4fc12601fa9246e8d411dca8aa8407334e8bcf
Amine Eleuch <amine.elleuch1@gmail.com>
2017-10-24 10:14:42 +0200

    start Project

--- app/src/main/java/com/example/justmaino/barcodebattler/MainActivity.java
+++ app/src/main/java/com/example/justmaino/barcodebattler/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.justmaino.barcodebattler;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView helloTxt;
-    Button nextBtn;
+    Button playBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,29 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         helloTxt = (TextView)findViewById(R.id.helloTxt);
-        nextBtn = (Button)findViewById(R.id.nextBtn);
-
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent next= new Intent(MainActivity.this,YesNoActivity.class);
-                next.putExtra("next","next");
-                startActivityForResult(next,1);
-            }
-        });
-
-
+        playBtn = (Button)findViewById(R.id.playBtn);
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        System.out.println("requestCode : "+requestCode);
             if (resultCode == 1) {
                 helloTxt.setText("Yes ,Bien Reçu");
             }
             else {
-                helloTxt.setText("!!!");
+                helloTxt.setText("No");
             }
     }
 
